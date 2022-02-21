d5baac469ce55dda8c3c0d68838da5b79d4eafde
FahimAdib <fahim.adib01@gmail.com>
2022-02-21 16:48:01 +0600

    Updates

---
+++ precache-manifest.95d2d1a90b8b218b12b3844afe2c4b79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f31e9f34ea7342d9083a1b524ee7e517",
+    "url": "/doubletap/index.html"
+  },
+  {
+    "revision": "389230f9860274b961d5",
+    "url": "/doubletap/static/css/main.c574d6b2.chunk.css"
+  },
+  {
+    "revision": "09c043210e17a5f32ead",
+    "url": "/doubletap/static/js/2.b3721e62.chunk.js"
+  },
+  {
+    "revision": "389230f9860274b961d5",
+    "url": "/doubletap/static/js/main.72432d54.chunk.js"
+  },
+  {
+    "revision": "1c8d12c3a2257f52a35b",
+    "url": "/doubletap/static/js/runtime~main.170c4840.js"
+  },
+  {
+    "revision": "a41ea85a57f1e5b5f8e261fd858d1ead",
+    "url": "/doubletap/static/media/1.a41ea85a.jpg"
+  }
+]);
